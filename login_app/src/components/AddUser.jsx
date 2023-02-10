7d69522bc9c961cc5ed05945e1efd57599b4f488
js <js@cos.pl>
2023-02-10 15:10:50 +0100

    Add two components and module csses

--- login_app/src/components/AddUser.jsx
+++ login_app/src/components/AddUser.jsx
@@ -1,8 +1,11 @@
 import React from 'react'; 
+import Card from '../UI/Card';
+import classes from './AddUser.module.css' ;
 
 function AddUser() {
 
     return (
+        <Card className={classes.input}>
         <form >
             <label htmlFor="username">Username</label>
             <input id="username" type="text "/>
@@ -12,6 +15,7 @@ function AddUser() {
 
             <button type="submit"> Add user </button>
         </form>
+        </Card>
     );
 }
 
