27232a9fb0faa49ed2686dfe80c734ce1f4a0df7
js <js@cos.pl>
2023-02-16 15:19:48 +0100

    Add Button component. Add tw event handler.

--- login_app/src/components/AddUser.jsx
+++ login_app/src/components/AddUser.jsx
@@ -1,19 +1,45 @@
-import React from 'react'; 
+import React, { useState} from 'react'; 
 import Card from '../UI/Card';
 import classes from './AddUser.module.css' ;
+import Button from '../UI/Button';
 
 function AddUser() {
 
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+
+    function namedChangeHandler(event){
+        setName(event.target.value)
+        
+    }
+    function ageChangeHandler(event){
+        setAge(event.target.value);
+        console.log(age)
+    }
+
+    function addUserHandler(event){
+        event.preventDefault();
+        console.log(name, age);
+        setAge('');
+        setName('');
+    }
+
     return (
         <Card className={classes.input}>
-        <form >
+        <form onSubmit={addUserHandler} >
             <label htmlFor="username">Username</label>
-            <input id="username" type="text "/>
+            <input id="username" type="text "
+                   onChange={namedChangeHandler} 
+                   value={name}
+            />
 
             <label htmlFor="age">Age</label>
-            <input id="age" type="Number" />
+            <input id="age" type="Number" 
+                    onChange={ageChangeHandler}
+                    value={age}
+            />
 
-            <button type="submit"> Add user </button>
+            <Button myType="submit"> Add user </Button>
         </form>
         </Card>
     );
